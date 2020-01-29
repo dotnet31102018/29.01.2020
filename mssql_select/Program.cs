4b75ba8f98dce969956f2c8056ebae03ff74dfac
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2020-01-29 18:53:13 +0200

    Add files via upload

---
+++ mssql_select/Program.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePr
+{
+    class Program
+    {
+        public class Employee
+        {
+            public int Id { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Gender { get; set; }
+            public int Salary { get; set; }
+            public override string ToString()
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+        }
+        private static List<object> SelectAll(string tableName)
+        {
+            //Command and Data Reader
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["employeedb"].ConnectionString);
+            cmd.Connection.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $"SELECT * FROM {tableName}";
+
+            SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+            List<Object> list = new List<object>();
+            while (reader.Read() == true)
+            {
+                var e = new
+                {
+                    Id = reader["ID"],
+                    firaName = reader["FIRSTNAME"]
+                };
+                list.Add(e);
+            }
+
+            cmd.Connection.Close();
+
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            var result = SelectAll("Employees");
+            result.ForEach(e => Console.WriteLine(e));
+        }
+    }
+}
